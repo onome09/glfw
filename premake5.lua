968977f320fc861a72185ceade1c449a7047c635
Onome Oyibo <onome09@hotmail.com>
2019-12-29 02:28:30 +0000

    tidied up

--- premake5.lua
+++ premake5.lua
@@ -1,46 +1,50 @@
 project "GLFW"
+	kind "StaticLib"
+	language "C"
+	staticruntime "On"
 
-kind "StaticLib"
-language "C"
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-targetdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	files
+	{
+	   "include/GLFW/glfw3.h",
+	   "include/GLFW/glfw3native.h",
+	   "src/context.c",
+	   "src/init.c",
+	   "src/input.c",
+	   "src/monitor.c",
+	   "src/vulkan.c",
+	   "src/window.c"
+	}
 
-files
-{
-   "include/GLFW/glfw3.h",
-   "include/GLFW/glfw3native.h",
-   "src/context.c",
-   "src/init.c",
-   "src/input.c",
-   "src/monitor.c",
-   "src/vulkan.c",
-   "src/window.c"
-}
+	filter "system:windows"
+		systemversion "latest"
 
-filter "system:windows"
-buildoptions { "-std=c++11", "-lgdi32"}
-systemversion "10.0.17134.0"
-staticruntime "On"
+		files
+		{
+		   "src/win32_init.c",
+		   "src/win32_joystick.c",
+		   "src/win32_monitor.c",
+		   "src/win32_time.c",
+		   "src/win32_thread.c",
+		   "src/win32_window.c",
+		   "src/wgl_context.c",
+		   "src/egl_context.c",
+		   "src/osmesa_context.c"
+		}
 
-files
-{
-   "win32_init.c",
-   "win32_joystick.c",
-   "win32_monitor.c",
-   "win32_time.c",
-   "win32_thread.c",
-   "win32_window.c",
-   "wgl_context.c",
-   "egl_context.c",
-   "osmesa_context.c"
-}
+		defines
+		{
+		   "_GLFW_WIN32",
+		   "_CRT_SECURE_NO_WARNINGS"
+		}
 
-defines
-{
-   "_GLFW_WIN32"
-   "_CRT_SECURE_NO_WARNINGS"
-}
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
 
-filter { "system:windows", "configurations:Release" }
-buildoptions "/MT"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
+
